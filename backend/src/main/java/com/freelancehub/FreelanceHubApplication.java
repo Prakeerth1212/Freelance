304614f70349f4281818e6b1d09b9e90ddcab99f
Prakeerth1212 <prakeerth.gamer@gmail.com>
2026-07-28 12:41:06 +0530

    chore: add version constant

--- backend/src/main/java/com/freelancehub/FreelanceHubApplication.java
+++ backend/src/main/java/com/freelancehub/FreelanceHubApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FreelanceHubApplication {
+    public static final String VERSION = "1.0.0";
     public static void main(String[] args) {
         SpringApplication.run(FreelanceHubApplication.class, args);
     }
